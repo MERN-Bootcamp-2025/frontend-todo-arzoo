1611bd7fc1c3067bab3e843a4c60180decd28364
Arzoo Jain <arzoo.jain@mindbowser.com>
2025-08-09 15:00:29 +0530

    Added navbar(dashboard header) with admin and user role check for + icon and avatar, added dropdown on avtar for logout button

--- TodoBuudy/src/pages/DashBoardPage.tsx
+++ TodoBuudy/src/pages/DashBoardPage.tsx
@@ -1,29 +1,33 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {type RootState } from "../redux/store";
+import { type RootState } from "../redux/store";
 import { logout } from "../redux/authSlice";
 import Button from "../components/Button";
+import DashboardHeader from "../components/Header";
 
 const DashboardPage: React.FC = () => {
-  const { user } = useSelector((state: RootState) => state.auth);
-  const dispatch = useDispatch();
+    const { user } = useSelector((state: RootState) => state.auth);
+    const dispatch = useDispatch();
 
-  return (
-    <div className="p-6">
-      <h1 className="text-xl font-bold">Welcome to Dashboard</h1>
-      <p className="mt-2">ID: {user?.user}</p>
-      <p>Role: {user?.role}</p>
-      <Button
-        variant="danger"
-        onClick={() => {
-          dispatch(logout());
-        }}
-        className="mt-4"
-      >
-        Logout
-      </Button>
-    </div>
-  );
+    return (
+        <>
+            <DashboardHeader/>
+            <div className="p-6">
+                <h1 className="text-xl font-bold">Welcome to Dashboard</h1>
+                <p className="mt-2">ID: {user?.user}</p>
+                <p>Role: {user?.role}</p>
+                <Button
+                    variant="danger"
+                    onClick={() => {
+                        dispatch(logout());
+                    }}
+                    className="mt-4"
+                >
+                    Logout
+                </Button>
+            </div>
+        </>
+    );
 };
 
 export default DashboardPage;
